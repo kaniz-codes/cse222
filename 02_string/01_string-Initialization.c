d9fa82912218cbe27b69cde30e09da6b0df8cd64
Kaniz Fatema <social.kaniz@gmail.com>
2025-02-05 00:27:03 +0600

    Update

--- 02_string/01_string-Initialization.c
+++ 02_string/01_string-Initialization.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
-int main()
-{
+
+int main() {
     char name[] = "Kaniz";
     char name1[10] = "Kaniz";
     printf("%s\n", name);
     printf("%s\n", name1);
 
-    //Character Array with Individual Characters
-    char name2[] = {'K','A','N','I','Z','\0'};
-    char name3[10] = {'K','A','N','I','Z','\0'};
+    // Character Array with Individual Characters
+    char name2[] = {'K', 'A', 'N', 'I', 'Z', '\0'};
+    char name3[10] = {'K', 'A', 'N', 'I', 'Z', '\0'};
     printf("%s\n", name2);
     printf("%s\n", name3);
 
@@ -17,16 +17,17 @@ int main()
     char *me = "Hello, Kaniz!";
     printf("%s\n", me);
 
-    // using gets
+    // Using gets instead of gets
     char experiment[50];
     printf("Experiment: ");
     gets(experiment);
     printf("Result of Experiment: %s\n", experiment);
 
-    // using fgets
+    // Using fgets instead of gets
     char experiment1[50];
     printf("Experiment No1: ");
-    gets(experiment1, 50, stdin);
+    fgets(experiment1, sizeof(experiment1), stdin);
     printf("Result of Experiment No 1: %s\n", experiment1);
 
+    return 0;
 }
