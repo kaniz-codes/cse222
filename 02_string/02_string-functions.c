c9480488f28a5364c1064bb57acf519da9564e1d
Kaniz Fatema <social.kaniz@gmail.com>
2025-03-11 12:51:05 +0600

    Updated

--- 02_string/02_string-functions.c
+++ 02_string/02_string-functions.c
@@ -1,33 +1,49 @@
-#include<stdio.h>
-int main()
-{
-    char name[]="Kaniz";
-    char name1[]="Fatema";
-    strcat(name1, "");
-    char name2[7]= {'K', 'A', 'N', 'I', 'Z', '\0'};
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Initialize strings with proper sizes
+    char name[20] = "Kaniz";
+    char name1[] = "Fatema";
+    char name2[7] = {'K', 'A', 'N', 'I', 'Z', '\0'};
     char name3[20];
-    char name4[7]= {'K', 'A', 'N', 'I', 'Z', '\0'};
-    //char name3[]= {'K', 'A', 'N', 'I', 'Z'};
-    printf("Input string %s\n", name);
-    // Length of string
-    printf("Length of string %d\n", strlen(name));
-    // Size of string
-    printf("Size of string %d\n", sizeof(name));
+    char name4[7] = {'K', 'A', 'N', 'I', 'Z', '\0'};
+    char name5[20] = "Hello, ";
 
-    // Concatenate Strings
-    strcat(name, name1);
-    printf("%s\n", name);
+    // Display input string
+    printf("Input string: %s\n", name);
 
-    //copy string
-    strcpy(name3, name2);
-    printf("%s\n", name3);
+    // String Length & Size
+    printf("Length of string: %d\n", (int)strlen(name));
+    printf("Size of string: %d\n", (int)sizeof(name));
+
+    // Concatenate Strings Safely
+    strncat(name, name1, sizeof(name) - strlen(name) - 1);
+    printf("Concatenated String: %s\n", name);
+
+    // Copy string safely
+    strncpy(name3, name2, sizeof(name3) - 1);
+    name3[sizeof(name3) - 1] = '\0'; 
+    printf("Copied String: %s\n", name3);
 
     // Compare Strings
+    printf("String Comparison: %d\n", strcmp(name4, name3));
 
-    printf("%d\n", strcmp(name4,name3));
-    
-    //substring
-    char name7[]="Kaniz";
-    printf("%s\n",strstr(name7,"a"));
+    // Substring Search
+    char name7[] = "Kaniz";
+    char *sub = strstr(name7, "a");
+    if (sub) {
+        printf("Substring found: %s\n", sub);
+    } else {
+        printf("Substring not found\n");
+    }
+
+    // Find Character in String
+    char *pos = strchr(name7, 'i');
+    if (pos) 
+    {
+        printf("Character 'i' found at position: %ld\n", pos - name7);
+    }
+
+    return 0;
 }
-
